5be319b79ba26ebb8f482f217a26fe002e6ff59e
tyun90 <t.yun@live.com>
2015-04-09 01:10:14 -0700

    ItemDatabase
    Added ItemDatabase

---
+++ GUIScripts/Item.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item {
+
+	public GameObject obj;
+	public string itemName;
+	public string itemDescription;
+	public Quaternion examineRot;
+	public Sprite displayImg;
+
+	public Item(GameObject o, string n, Quaternion q, string msg, Sprite img) {
+		obj = o;
+		itemName = n;
+		examineRot = q;
+		itemDescription = msg;
+		displayImg = img;
+	}
+
+	public Item() {
+		obj = null;
+	}
+
+	public string name {
+		get {
+			return itemName;
+		}
+		set {
+			itemName = value;
+		}
+	}
+
+	public string description {
+		get {
+			return itemDescription;
+		}
+		set {
+			itemDescription = value;
+		}
+	}
+
+	public GameObject prefab {
+		get {
+			return obj;
+		}
+		set {
+			obj = value;
+		}
+	}
+
+	public Sprite sprite {
+		get {
+			return displayImg;
+		}
+		set {
+			displayImg = value;
+		}
+	}
+}
---
+++ GUIScripts/ItemDatabase.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+
+	private static ItemDatabase db;
+	private List<Item> items = new List<Item>();
+
+	public static ItemDatabase instance {
+		get {
+			if (db == null) {
+				db = GameObject.FindObjectOfType<ItemDatabase> ();
+			}
+			return db;
+		}
+	}
+
+	public void add(Item i) {
+		items.Add (i);
+	}
+
+	public List<Item> itemList() {
+		return items;
+	}
+
+	public string[] itemNames() {
+		string[] nameArray = new string[items.Count];
+		int index = 0;
+		foreach (Item i in items) {
+			//nameArray [index] = i.name;
+			nameArray[index] = itemNamesHelper (nameArray, i.name);
+			index++;
+		}
+		return nameArray;
+	}
+
+	private string itemNamesHelper(string[] s, string name) {
+		int count = 0;
+		foreach (string str in s) {
+			if (str != null && str.Substring(0,str.Length-2) == name) {
+				count++;
+			}
+		}
+		return name + " " + count;
+	}
+
+}
