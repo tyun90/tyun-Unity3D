6833166df6d1b35b585fdad0e99fae2c7dd401c1
tyun90 <t.yun@live.com>
2015-04-09 18:35:27 -0700

    ItemGUI
    Added pickup GUI

---
+++ GUIScripts/GUIItemText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIItemText : MonoBehaviour {
+
+	public string msg = "Press G to pick up";
+	public Text thisText;
+	public float initialDelay = 1f;
+	public float typeDelay = 0.00001f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Awake() {
+		//thisText = this.GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator Typing() {
+		yield return new WaitForSeconds(initialDelay);
+		for (int i = 0; i <= msg.Length; i++) {
+			thisText.text = msg.Substring(0,i);
+			yield return new WaitForSeconds(typeDelay);
+		}
+	}
+
+	IEnumerator ClearText() {
+		thisText.text = "";
+		yield return null;
+	}
+}
---
+++ GUIScripts/RotateToPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToPlayer : MonoBehaviour {
+
+	private Transform canvas;
+	public Transform player;
+
+	private Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotation = transform.position - player.position;
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (rotation), 10 * Time.deltaTime);
+
+	}
+}
