ce3f858e0786495fd93b7b70b391deece227d1bd
tyun90 <t.yun@live.com>
2015-04-09 19:23:28 -0700

    Pickups
    Pickup scripts added

---
+++ GUIScripts/PickUp.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+public class PickUp : MonoBehaviour {
+
+	public FullBodyBipedIK ik;
+	public Transform Hand;
+	public Transform newObj;
+
+	private bool isHeld;
+
+	private Transform itemInfoCanvas;
+	private Animator itemInfo;
+	public GUIItemText text;
+	public GUIItemText text2;
+
+	void Awake() {
+		itemInfoCanvas = transform.parent.FindChild ("GUI").FindChild("Canvas");
+		itemInfo = transform.parent.FindChild("GUI").FindChild("Canvas").GetComponent<Animator> ();
+		text = transform.parent.FindChild ("GUI").FindChild("Canvas").FindChild ("InfoFadeIn").GetComponentInChildren<GUIItemText> ();
+		text2 = transform.parent.FindChild ("GUI").FindChild("Canvas").FindChild ("ItemImage").GetComponentInChildren<GUIItemText> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		isHeld = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ik.solver.leftHandEffector.positionWeight > 0.99 && !isHeld) {
+			Vector3 scaling = transform.localScale;
+			InventoryManager.instance.itemPickedUp(transform.gameObject);
+			InventoryManager.instance.setOpen(true);
+			transform.gameObject.SetActive(false); 
+			itemInfoCanvas.gameObject.SetActive(false);
+
+			newObj.gameObject.SetActive(true);
+			newObj.SetParent(Hand);
+			newObj.localScale = scaling;
+			//newObj.localPosition = new Vector3(-0.09935392f,-0.3808476f,-0.6535484f);
+			//newObj.localRotation = new Quaternion(0,0,0,0);
+			//newObj.localRotation = new Quaternion(14.83635f, 52.90372f, 322.2132f,0);
+			//transform.localPosition = new Vector3(0,0,0);
+			isHeld = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			itemInfo.SetTrigger("FadeIn");
+			text.StartCoroutine("Typing");
+			text2.StartCoroutine("Typing");
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.tag == "Player") {
+			itemInfo.SetTrigger("FadeOut");
+			text.StartCoroutine("ClearText");
+			text2.StartCoroutine("ClearText");
+		}
+	}
+}
---
+++ GUIScripts/PickUpCamera.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+[RequireComponent(typeof(InteractionSystem))]
+public class PickUpCamera : MonoBehaviour {
+
+	public Camera MainCamera;
+	public CharacterController temp;
+	[SerializeField] InteractionObject interactionObject; // The object to interact to
+	[SerializeField] FullBodyBipedEffector[] effectors; // The effectors to interact with
+	private InteractionSystem interactionSystem;
+
+	public FullBodyBipedIK ik;
+	public LookAtIK lookTarget;
+	public Transform head;
+
+	public bool pickedUp;
+	public bool nearItem;
+	public Transform targetItem;
+	public Transform targetMove;
+
+	public Vector3 x = new Vector3(0,0,0);
+
+	private Transform[] destroyItems;
+
+	private Vector3 lookAtPickup;
+
+	void Awake() {
+		lookTarget.Disable ();
+		interactionSystem = GetComponent<InteractionSystem>();
+		pickedUp = false;
+		nearItem = false;
+		destroyItems = new Transform[2];
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//x.x = Screen.width / 2;
+		//x.y = Screen.height / 2;
+
+		//RaycastHit inRange;
+
+		//if (Physics.Raycast(MainCamera.ViewportPointToRay(x), out inRange, 5)) {
+		//	if (inRange.distance <= 5 && inRange.collider.gameObject.tag == "PickUp") {
+		//		Debug.Log ("Near");
+		//		nearItem = true;
+		//		lookTarget.solver.IKPosition = inRange.collider.transform.position;
+
+		//	}
+		//} else {
+		//	nearItem = false;
+		//}
+
+
+		if (Input.GetKeyDown(KeyCode.G) && nearItem) {
+			ik.enabled = true;
+			nearItem = false;
+			//lookTarget.solver.IKPosition = lookAtPickup;
+			foreach (FullBodyBipedEffector e in effectors) {
+				interactionSystem.StartInteraction(e, interactionObject, true);
+			}
+			pickedUp = true;
+		}	
+
+		//If Camera is picked up, move camera to target position in front of face
+		if (!interactionSystem.inInteraction && pickedUp){
+
+			//Smoothing, don't know why there's stutter if this is ommitted.
+			if (targetItem.parent.name != "EYE_DEF") {
+				targetItem.localPosition = new Vector3(.06f,0,0);
+			}
+
+			//Reset LookAt Weight to 1f because pick up sets it to 0
+			lookTarget.solver.SetLookAtWeight(1f, 0.5f, 1f, 1f, 0.5f, 0.5f, 0.5f);
+
+			targetItem.SetParent(head.FindChild("EYE_DEF"));
+			targetItem.localPosition = Vector3.Slerp(targetItem.localPosition,targetMove.localPosition, 10f * Time.deltaTime);
+			targetItem.localRotation = Quaternion.Lerp(targetItem.localRotation, targetMove.localRotation, 10f * Time.deltaTime);
+
+			//If hand and camera are almost in position, set the position to the default target position
+			if (Mathf.Abs(targetItem.localPosition.x - targetMove.localPosition.x) < 0.001) {
+				targetItem.localPosition = targetMove.localPosition;
+				targetItem.localRotation = targetMove.localRotation;
+				targetMove.gameObject.SetActive(true);
+				GameObject.Find("First Person Controller").transform.FindChild("unitychan").GetComponent<HeadRotation>().targetHand = targetMove.transform.FindChild("DefaultPos").transform;
+				pickedUp = false;
+				Destroy (targetItem.gameObject);
+				//Destroy (destroyItems[0].gameObject);
+				Destroy (destroyItems[1].gameObject);
+			}
+		}
+	}
+	
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "PickUp") {
+			nearItem = true;
+			interactionObject = col.transform.GetComponent<InteractionObject>();
+			targetItem = col.transform.parent.GetChild(1);
+			//destroyItems[0] = col.transform;
+			destroyItems[1] = col.transform.parent;
+			lookAtPickup = col.transform.parent.position;
+			col.transform.parent = null;
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+			nearItem = false;
+	}
+
+}
